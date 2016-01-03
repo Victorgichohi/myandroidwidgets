19ae377b54d2f528978c99c95d1be38062cf4fd3
Victorgichohi <victorgichohi48@gmail.com>
2016-01-04 00:04:55 +0300

    added the splash activity which after loading should parse the data

--- RssReader/app/src/main/java/codescratch/rssreader/SplashActivity.java
+++ RssReader/app/src/main/java/codescratch/rssreader/SplashActivity.java
@@ -1,52 +1,81 @@
 package codescratch.rssreader;
 
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AlertDialog;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
+
+    private String RSSFEEDURL = "http://www.mobilenations.com/rss/mb.xml";
+    RSSFeed feed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+        setContentView(R.layout.splash);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        ConnectivityManager conMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (conMgr.getActiveNetworkInfo() == null
+                && !conMgr.getActiveNetworkInfo().isConnected()
+                && !conMgr.getActiveNetworkInfo().isAvailable()) {
+// No connectivity - Show alert
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(
+                    "Unable to reach server, \nPlease check your connectivity.")
+                    .setTitle("TD RSS Reader")
+                    .setCancelable(false)
+                    .setPositiveButton("Exit",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog,
+                                                    int id) {
+                                    finish();
+                                }
+                            });
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            AlertDialog alert = builder.create();
+            alert.show();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        } else {
+// Connected - Start parsing
+            new AsyncLoadXMLFeed().execute();
+
         }
 
-        return super.onOptionsItemSelected(item);
+    }
+
+    private class AsyncLoadXMLFeed extends AsyncTask {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+// Obtain feed
+            DOM_Parser myParser = new DOM_Parser();
+            feed = myParser.parseXml(RSSFEEDURL);
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+
+            Bundle bundle = new Bundle();
+            bundle.putSerializable("feed", feed);
+
+// launch List activity
+            Intent intent = new Intent(SplashActivity.this, ListActivity.class);
+            intent.putExtras(bundle);
+            startActivity(intent);
+
+// kill this activity
+            finish();
+        }
     }
 }
