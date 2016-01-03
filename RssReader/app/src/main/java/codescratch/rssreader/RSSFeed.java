a26ecca095e79f0db3d1d8cb07b6059ad8ac19bf
Victorgichohi <victorgichohi48@gmail.com>
2016-01-03 23:53:29 +0300

    added the rss item class which contais a list of all items being parsed

---
+++ RssReader/app/src/main/java/codescratch/rssreader/RSSFeed.java
@@ -0,0 +1,32 @@
+package codescratch.rssreader;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Vector;
+
+/**
+ * Created by victor on 1/3/16.
+ */
+public class RSSFeed implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private int _itemcount = 0;
+    private List<RSSItem> _itemlist;
+
+    RSSFeed() {
+        _itemlist = new Vector<RSSItem>(0);
+    }
+
+    void addItem(RSSItem item) {
+        _itemlist.add(item);
+        _itemcount++;
+    }
+
+    public RSSItem getItem(int location) {
+        return _itemlist.get(location);
+    }
+
+    public int getItemCount() {
+        return _itemcount;
+    }
+}
